f70dabe696ae579fe2837509eb6ead97b68f7445
Tarek Ibrahim <tarek.ibrahim@wartsila.com>
2021-08-26 02:20:47 +0300

    minor cost function improvements

--- gym_custom/envs/cartpole_custom_torch.py
+++ gym_custom/envs/cartpole_custom_torch.py
@@ -107,9 +107,10 @@ class CartPoleEnv(gym.Env):
     
     def cost_o(self,o,tensor=True):
         if tensor:
-            return torch.abs(o[:,2:3]).sum(dim=1)
-            # return -(-((torch.cat([o[:,:1]-self.length*o[:,2:3].sin(),-self.length*(o[:,2:3].cos()+1)],dim=1)/self.length).pow(2)).sum(dim=1)).exp()
+            return torch.abs(o[:,2:3]).sum(dim=1) #+ 0.01 * torch.abs(o[:,:1]).sum(dim=1)
+            # return -(-((torch.cat([o[:,:1]-self.length*o[:,2:3].sin(),-self.length*o[:,2:3].cos()-self.length],dim=1)/self.length).pow(2)).sum(dim=1)).exp()
         else:
+            # return np.sum(np.abs(o[2]))
             return -np.exp(-np.sum((np.array([o[0]-self.length*np.sin(o[2]),-self.length*(np.cos(o[2])+1)])/self.length)**2))
 
     
@@ -118,6 +119,7 @@ class CartPoleEnv(gym.Env):
             return torch.zeros(a.shape[0],device=self.device)
             # return 0.01 * a.pow(2).sum(dim=1)
         else:
+            # return 0.0
             return 0.01 * np.sum(a**2)
 
 
